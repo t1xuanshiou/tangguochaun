e402c3b0ff27c5934ddea7a6dbf5166be64ace94
tangguochuan <2015966151@qq.com>
2023-11-25 17:57:38 +0800

    written by hakimi

--- whac-a-mole.c
+++ whac-a-mole.c
@@ -1,41 +1,59 @@
 //
 // Created by LENOVO on 2023/11/19.
 //
-#include <stdio.h>
-int board[700][700];int maxarr[700][2];
-int n,m,d;
-int totalstorage[700][700];
-int total(int row,int col,int s){
-  if(s==1){return board[row][col]+board[row-1][col]+board[row+1][col]+board[row][col-1]+board[row][col+1];}
-  int sum=0;
-  for(int i=0;i<=s;i++){
-    sum+=board[row-s+i][col+i]+board[row+i][col-s+i]+board[row-i][col-s+i]+board[row+s-i][col+i];
-  }
-    return sum-board[row][col-s]-board[row][col+s]-board[row+s][col]-board[row-s][col]+ total(row,col,s-1);
-}
+#include<stdio.h>
+int map[1005][1005];
+int px[100005], py[100005], cnt;
 int main(){
-  scanf("%d%d%d",&n,&m,&d);
-  for(int i=50;i<50+n;i++){
-    for(int j=50;j<50+m;j++){
-      scanf("%d",&board[i][j]);
+  const int offset = 100;
+  int n, m, d;
+  scanf("%d%d%d", &n, &m, &d);
+  for(int i = 1; i <= n; i++){
+    for(int j = 1; j <= m; j++){
+      int x = i + offset;
+      int y = j + offset;
+      scanf("%d", &map[x][y]);
     }
   }
-  int max= total(50,50,d);
-  for(int i=50;i<50+n;i++){
-    for(int j=50;j<50+m;j++){
-      totalstorage[i][j]= total(i,j,d);
-      if(total(i,j,d)>max){max= totalstorage[i][j];}
+  int sum, maxn = -0x7fffffff;
+  for(int i = 1; i <= n; i++){
+    int x = i + offset;
+    sum = 0;
+    for(int j = 0; j <= d; j++){
+      for(int k = j - d; k <= d - j; k++){
+        sum += map[x + k][j + 1 + offset];
+      }
     }
-  }int count=0;
-  for(int i=50;i<50+n;i++){
-    for(int j=50;j<50+m;j++){
-      if(totalstorage[i][j]==max){maxarr[count][0]=i-49;maxarr[count][1]=j-49;count++;}
+    if(sum >= maxn){
+      if(sum > maxn){
+        cnt = 0;
+        maxn = sum;
+      }
+      px[++cnt] = i;
+      py[cnt] = 1;
+    }
+    for(int j = 2; j <= m; j++){
+      for(int k = 0; k <= d; k++){
+        sum -= map[x + d - k][j + offset - k - 1];
+        sum += map[x + d - k][j + offset + k];
+        if(d != k){
+          sum -= map[x - d + k][j + offset - k - 1];
+          sum += map[x - d + k][j + offset + k];
+        }
+      }
+      if(sum >= maxn){
+        if(sum > maxn){
+          cnt = 0;
+          maxn = sum;
+        }
+        px[++cnt] = i;
+        py[cnt] = j;
+      }
     }
   }
-  printf("%d %d\n", max,count);
-  for(int i=0;i<count;i++){
-    printf("%d %d",maxarr[i][0],maxarr[i][1]);
-    printf("\n");
+  printf("%d %d\n", maxn, cnt);
+  for(int i = 1; i <= cnt; i++){
+    printf("%d %d\n", px[i], py[i]);
   }
   return 0;
 }
