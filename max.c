62f18a8485df5dc8ea54581c0251e8047d729cee
tangguochuan <2015966151@qq.com>
2023-11-28 20:23:49 +0800

    递归写法

--- max.c
+++ max.c
@@ -2,42 +2,40 @@
 // Created by LENOVO on 2023/11/13.
 //
 #include <stdio.h>
-#define MAX_SIZE 100
-
-// 定义一个全局的数组来模拟栈
-int stack[MAX_SIZE];
-
-// 定义一个全局的变量作为栈顶的指针
-int top = -1;
-
-// push函数
-void stackpush(int value) {
-  if(top >= MAX_SIZE - 1) {
-    printf("栈已满，无法添加元素\n");
-  } else {
-    top++; // 移动栈顶指针
-    stack[top] = value; // 在栈顶放入新元素
+int input[100001];
+int findstorage[100001][2];
+void find(int l,int r){
+  if(r<l){
+    return;}
+  if(l==r){
+    findstorage[l][0]=l;
+    findstorage[l][1]=l;
+  }
+  else{
+    int max=-1;
+    int pos=-1;
+    for(int i=l;i<=r;i++){
+      if(input[i]>max){
+        max=input[i];
+        pos=i;
+      }
+    }
+    findstorage[pos][0]=l;
+    findstorage[pos][1]=r;
+    find(l,pos-1);
+    find(pos+1,r);
   }
 }
-
-// pop函数
-int stackpop() {
-  if(top == -1) {
-    printf("栈为空，无法取出元素\n");
-    return -1; // 返回一个错误值
-  } else {
-    int value = stack[top]; // 取出栈顶元素
-    top--; // 移动栈顶指针
-    return value;
+int n;
+int main(){
+  scanf("%d",&n);
+  for(int i=1;i<=n;i++){
+    scanf("%d",&input[i]);
   }
-}
-
-int main() {
-  stackpush(5);
-  stackpush(10);
-  stackpush(15);
-  printf("%d\n", stackpop()); // 输出：15
-  printf("%d\n", stackpop()); // 输出：10
-  printf("%d\n", stackpop()); // 输出：5
+  find(1,n);
+  for(int i=1;i<=n;i++){
+    printf("%d %d",findstorage[i][0],findstorage[i][1]);
+    printf("\n");
+  }
   return 0;
 }
